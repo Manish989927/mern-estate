5f7563cdcb043c6fb59300f90e6a2d1308ca4643
Manish <Manish989927.com>
2024-02-25 16:43:42 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -26,6 +26,7 @@ app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
+        success: false,
         statusCode,
         message,
     });
